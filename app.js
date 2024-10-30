193a273a3fa69d86233bf4832196042c6d5889c2
saadarif626 <saadarif626@gmail.com>
2024-10-30 23:04:40 +0500

    Days counter!

---
+++ app.js
@@ -0,0 +1,21 @@
+document.getElementById("enddate").innerText=enddate;
+var inputs=document.querySelectorAll("input")
+
+var enddate="28 February 2025 12:00 AM"
+function clock(){
+    var end=new Date(enddate)
+    var now= new Date();
+    var diff=(end- now)/1000;
+   inputs[0].value=(Math.floor(diff/3600/24))
+   inputs[1].value=(Math.floor(diff/3600)%24)
+   inputs[2].value=(Math.floor(diff/60)%60)
+   inputs[3].value=(Math.floor(diff%60))
+}
+clock();
+
+setInterval(
+    ()=>{
+        clock()
+    },
+    1000
+)
